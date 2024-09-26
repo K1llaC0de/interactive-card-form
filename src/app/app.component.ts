da93bed3a7fc21756444c70c11747066cfbdb432
K1llaC0de <lauracode00@gmail.com>
2024-09-26 13:31:36 +0200

    initial project

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CardComponent } from './card/card.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, CardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {}
